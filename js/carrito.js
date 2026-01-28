5f3331235a8e9c4cf52108bc6a6182a6c28831ef
adriangb46 <adriangb46@educastur.es>
2026-01-28 16:06:17 +0100

    .

--- js/carrito.js
+++ js/carrito.js
@@ -120,7 +120,7 @@
 
   function checkout() {
     alert("Redirigiendo a la página de pago...");
-    localStorage.setItem("lastBuyCart",cartHasher(getCart());
+    localStorage.setItem("lastBuyCart",cartHasher(getCart()));
     // window.location.href = "/checkout.html";
   }
 
