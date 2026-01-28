4d9b92f0cad3dc1462995501efa4cb0b9ce5ee2d
adriangb46 <adriangb46@educastur.es>
2026-01-28 13:55:08 +0100

    .

--- js/carrito.js
+++ js/carrito.js
@@ -159,7 +159,7 @@
         const exists = cart.find(p => p.id == item.id);
         if (exists) {
             console.log("Vamos por buen lugar");
-            exists.quantity =  parseInt(exists.quantity) += 1;
+            exists.quantity =  parseInt(exists.quantity) + 1;
         } else {
             cart.push(item);
         }
