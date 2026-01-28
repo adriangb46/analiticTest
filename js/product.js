2560ec62997d7303fcaa613bf910c41340be424a
adriangb46 <adriangb46@educastur.es>
2026-01-28 11:12:17 +0100

    .

--- js/product.js
+++ js/product.js
@@ -100,7 +100,7 @@ function handlePurchase(product) {
     s.tl(this, 'o', "producto Comprado")
 */
     // Show success message
-    addToCart({
+    addItem({
         name:product.name,
         price:product.price,
         category:product.category
